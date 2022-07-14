c032817a843b41c3d2062af2474967865546d583
DeysiLopes <deysilinconl@gmail.com>
2022-07-14 15:26:48 -0300

    Create Header component

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+function Header() {
+
+    return (
+        <div className="dsmeta-logo-container">
+        <img src="logo.svg" alt="DSMeta" />
+        <h1>DSMeta</h1>
+        <p>
+          Desenvolvido por
+          <a href="https://github.com/DeysiLopes">DeysiLopes</a>
+        </p>
+    </div>
+    )
+  }
+  
+export default Header
