ff251ad25580a0c06607b090dcd029acaedeccf6
DeysiLopes <deysilinconl@gmail.com>
2022-07-14 20:37:41 -0300

    Create repositories in the app

---
+++ backend/src/main/java/com/deysi/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.deysi.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.deysi.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
